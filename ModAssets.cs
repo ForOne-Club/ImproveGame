6f0ff69237ac256f138495ac4039d44a2ad3539b
局长 <487666123@qq.com>
2022-07-10 19:55:06 +0800

    什么都没改，格式化问题

--- ModAssets.cs
+++ ModAssets.cs
@@ -9,16 +9,20 @@ namespace ImproveGame
         internal static Asset<Effect> LiquidSurface;
         internal static Asset<Texture2D> Perlin;
 
-        public override void Load() {
-            if (!Main.dedServ) {
+        public override void Load()
+        {
+            if (!Main.dedServ)
+            {
                 ItemEffect = MyUtils.GetEffect("item");
                 LiquidSurface = MyUtils.GetEffect("LiquidSurface");
                 Perlin = Main.Assets.Request<Texture2D>("Images/Misc/Perlin");
             }
         }
 
-        public override void Unload() {
-            if (!Main.dedServ) {
+        public override void Unload()
+        {
+            if (!Main.dedServ)
+            {
                 ItemEffect = null;
                 LiquidSurface = null;
                 Perlin = null;
