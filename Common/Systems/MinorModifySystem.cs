c78ae7719177f264633fbb47f6a3e505c0364642
-Cyril- <59193481@qq.com>
2022-07-31 17:47:12 +0800

    便携制作站

--- Common/Systems/MinorModifySystem.cs
+++ Common/Systems/MinorModifySystem.cs
@@ -56,6 +56,93 @@ namespace ImproveGame.Common.Systems
             On.Terraria.Chest.SetupShop += RefreshTravelShop;
             // 修改旗帜需求
             On.Terraria.NPC.CountKillForBannersAndDropThem += NPC_CountKillForBannersAndDropThem;
+            // 便携制作站
+            IL.Terraria.Player.AdjTiles += AddPortableStations;
+        }
+
+        private void AddPortableStations(ILContext il)
+        {
+            var c = new ILCursor(il);
+            if (!c.TryGotoNext(MoveType.Before, i => i.MatchLdsfld<Main>(nameof(Main.playerInventory))))
+                return;
+            c.Emit(OpCodes.Ldarg_0);
+            c.EmitDelegate<Action<Player>>((player) =>
+            {
+                if (!Config.PortableCraftingStation)
+                    return;
+
+                // 一系列的Station链条操作，每个tileID都要经历一次的
+                static void CheckChainedStations(int tileType, Player player)
+                {
+                    player.adjTile[tileType] = true;
+                    if (TileID.Sets.CountsAsWaterSource[tileType])
+                    {
+                        player.adjWater = true;
+                    }
+                    if (TileID.Sets.CountsAsLavaSource[tileType])
+                    {
+                        player.adjLava = true;
+                    }
+                    if (TileID.Sets.CountsAsHoneySource[tileType])
+                    {
+                        player.adjHoney = true;
+                    }
+                    switch (tileType)
+                    {
+                        case TileID.Hellforge:
+                        case TileID.GlassKiln:
+                            player.adjTile[TileID.Furnaces] = true;
+                            break;
+                        case TileID.AdamantiteForge:
+                            player.adjTile[TileID.Furnaces] = true;
+                            player.adjTile[TileID.Hellforge] = true;
+                            break;
+                        case TileID.MythrilAnvil:
+                            player.adjTile[TileID.Anvils] = true;
+                            break;
+                        case TileID.BewitchingTable:
+                        case TileID.Tables2:
+                        case TileID.PicnicTable:
+                            player.adjTile[TileID.Tables] = true;
+                            break;
+                        case TileID.AlchemyTable:
+                            player.adjTile[TileID.Bottles] = true;
+                            player.adjTile[TileID.Tables] = true;
+                            player.alchemyTable = true;
+                            break;
+                    }
+                    TileLoader.AdjTiles(player, tileType);
+                }
+
+                var items = GetAllInventoryItemsList(player, false);
+                foreach (var item in items)
+                {
+                    int tileType = item.createTile;
+                    if (tileType > -1 && tileType < TileLoader.TileCount)
+                    {
+                        CheckChainedStations(tileType, player);
+                    }
+                    if (ModIntegrationsSystem.PortableStations.TryGetValue(item.type, out var tileIDs))
+                    {
+                        foreach (int tileID in tileIDs)
+                        {
+                            CheckChainedStations(tileID, player);
+                        }
+                    }
+                    if (item.type == ItemID.WaterBucket || item.type == ItemID.BottomlessBucket)
+                    {
+                        player.adjWater = true;
+                    }
+                    if (item.type == ItemID.LavaBucket || item.type == ItemID.BottomlessLavaBucket)
+                    {
+                        player.adjLava = true;
+                    }
+                    if (item.type == ItemID.HoneyBucket /*|| item.type == ItemID.BottomlessHoneyBucket*/)
+                    {
+                        player.adjHoney = true;
+                    }
+                }
+            });
         }
 
         private void NPC_CountKillForBannersAndDropThem(On.Terraria.NPC.orig_CountKillForBannersAndDropThem orig, NPC npc)
--- README.md
+++ README.md
@@ -8,13 +8,22 @@
 # Mod.Call
 ## IgnoreInfItem
 ```"IgnoreInfItem", int/List<int> ItemID(s)```
+
 为某个/些指定的物品添加无尽增益忽略，以防止在拥有30个时无限提供增益
 ## AddPotion
 ```"AddPotion", int ItemID, int BuffID```
+
 为某个指定的物品添加无尽增益支持(药水类)，对于没有设置Item.buffType或拥有多个增益的物品十分有用，药水类指需要达到30堆叠才能提供增益
 ## AddStation
-```"AddPotion", int ItemID, int BuffID```
+```"AddStation", int ItemID, int BuffID```
+
 为某个指定的物品添加无尽增益支持(放置站类)，放置站类指有一个即可提供增益。放置站一般需要手动提供支持
+## AddPortableCraftingStation
+```"AddPortableCraftingStation", int ItemID, int/List<int> TileID(s)```
+
+为某个指定的物品添加便携制作站支持，对于没有设置Item.createTile但是应该充当某种制作站的物品十分有用，你可以指定多个制作站。
+
+如果你想要使其充当水源，应将TileID设置为水槽 (TileID.Sink)
 
 ## 使用例
 以下是一个为自己的放置站添加支持的例子
