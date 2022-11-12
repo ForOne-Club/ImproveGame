2e4141fafdf56df67c746eea48c826aa35ee5a09
Cyrillya <59193481@qq.com>
2022-11-12 18:27:34 +0800

    无尽Buff大改

--- Common/Players/InfBuffPlayer.cs
+++ Common/Players/InfBuffPlayer.cs
@@ -4,8 +4,25 @@ using ImproveGame.Content.Items;
 
 namespace ImproveGame.Common.Players
 {
+    /// <summary>
+    /// 22/11/9 无尽Buff流程<br/>
+    /// 1) 每隔 <see cref="SetupBuffListCooldownTime"/> 会更新一次Buff列表 <see cref="AvailableItems"/><br/>
+    /// 2) 每帧遍历 <see cref="AvailableItems"/> 对于所有 <see cref="CheckInfBuffEnable"/> 为 <see langword="true"/> 的Buff实现效果<br/>
+    /// 节省性能
+    /// </summary>
     public class InfBuffPlayer : ModPlayer
     {
+        /// <summary>
+        /// 用于记录玩家总获取的的无尽Buff物品
+        /// </summary>
+        internal List<Item> AvailableItems = new();
+
+        /// <summary>
+        /// 每隔多久统计一次Buff
+        /// </summary>
+        public static int SetupBuffListCooldown;
+        public const int SetupBuffListCooldownTime = 180;
+
         public static InfBuffPlayer Get(Player player) => player.GetModPlayer<InfBuffPlayer>();
         public static bool TryGet(Player player, out InfBuffPlayer modPlayer) => player.TryGetModPlayer(out modPlayer);
 
@@ -13,25 +30,92 @@ namespace ImproveGame.Common.Players
             On.Terraria.Player.AddBuff += BanBuffs;
         }
 
+        public override void OnEnterWorld(Player player)
+        {
+            SetupBuffList(player);
+        }
+
         public override void PostUpdateBuffs() {
             if (Player.whoAmI != Main.myPlayer || Main.netMode == NetmodeID.Server)
                 return;
-            HandleBuff(Player);
+
+            #region 应用增益
+            
+            foreach (var item in AvailableItems)
+            {
+                // 侏儒特判
+                if (item.createTile is TileID.GardenGnome)
+                    HideBuffSystem.HasGardenGnome = true;
+
+                int buffType = item.buffType;
+
+                // 饱食三级Buff不应该覆盖，而是取最高级
+                bool wellFed3Enabled = Main.LocalPlayer.FindBuffIndex(BuffID.WellFed3) != -1;
+                bool wellFed2Enabled = Main.LocalPlayer.FindBuffIndex(BuffID.WellFed2) != -1;
+
+                if (!CheckInfBuffEnable(buffType))
+                    continue;
+
+                // Buff
+                switch (buffType)
+                {
+                    case BuffID.WellFed when wellFed2Enabled || wellFed3Enabled:
+                    case BuffID.WellFed2 when wellFed3Enabled:
+                        continue;
+                    case -1:
+                        break;
+                    default:
+                        Main.LocalPlayer.AddBuff(buffType, 2);
+                        break;
+                }
+
+                // Buff站效果设置
+                if (!Config.NoPlace_BUFFTile)
+                    return;
+
+                switch (buffType)
+                {
+                    case BuffID.Campfire:
+                        HideBuffSystem.HasCampfire = true;
+                        break;
+                    case BuffID.HeartLamp:
+                        HideBuffSystem.HasHeartLantern = true;
+                        break;
+                    case BuffID.StarInBottle:
+                        HideBuffSystem.HasStarInBottle = true;
+                        break;
+                    case BuffID.Sunflower:
+                        HideBuffSystem.HasSunflower = true;
+                        break;
+                }
+            }
+            
+            #endregion
+
+            SetupBuffListCooldown++;
+            if (SetupBuffListCooldown % SetupBuffListCooldownTime != 0)
+                return;
+                
+            SetupBuffList(Player);
             if (Config.ShareInfBuffs)
-                CheckTeamPlayers(Player.whoAmI, HandleBuff);
+                CheckTeamPlayers(Player.whoAmI, SetupBuffList);
         }
 
+        #region 设置增益列表
+
         /// <summary>
-        /// 加无尽Buff
+        /// 设立无尽Buff列表
         /// </summary>
         /// <param name="player">从某个玩家身上获取物品列表</param>
-        public static void HandleBuff(Player player)
+        public void SetupBuffList(Player player)
         {
+            Get(player).AvailableItems.Clear();
+
             var items = GetAllInventoryItemsList(player, false);
             foreach (var item in items)
             {
                 HandleBuffItem(item);
-                if (!item.IsAir && item.ModItem is not null && item.ModItem is PotionBag potionBag && potionBag.storedPotions.Count > 0)
+                if (!item.IsAir && item.ModItem is PotionBag potionBag && potionBag.storedPotions.Count > 0)
                 {
                     foreach (var p in potionBag.storedPotions)
                     {
@@ -40,50 +124,16 @@ namespace ImproveGame.Common.Players
                 }
             }
         }
-
-        // 由于多人模式共享选项，这里原有的Player改成了Main.LocalPlayer，然后用了static
-        public static void HandleBuffItem(Item item) {
+        
+        public void HandleBuffItem(Item item) {
+            // 增益物品
             int buffType = ApplyBuffItem.GetItemBuffType(item);
-            if (buffType is not -1 && CheckInfBuffEnable(buffType)) {
-                // 饱食三级Buff不应该覆盖，而是取最高级
-                bool wellFed3Enabled = Main.LocalPlayer.FindBuffIndex(BuffID.WellFed3) != -1;
-                if (buffType == BuffID.WellFed && (Main.LocalPlayer.FindBuffIndex(BuffID.WellFed2) != -1 || wellFed3Enabled))
-                    return;
-                if (buffType == BuffID.WellFed2 && wellFed3Enabled)
-                    return;
-                if (item.type == ItemID.LuckPotionLesser || item.type == ItemID.LuckPotion || item.type == ItemID.LuckPotionGreater)
-                {
-                    Main.LocalPlayer.AddBuff(buffType, item.buffTime - 1);
-                    return;
-                }
-                Main.LocalPlayer.AddBuff(buffType, 2);
+            if (buffType is not -1 || item.createTile is TileID.GardenGnome) {
+                AvailableItems.Add(item);
             }
-            if (Config.NoPlace_BUFFTile)
-                HandleSceneMetrics(item);
         }
 
-        public static void HandleSceneMetrics(Item item)
-        {
-            switch (item.createTile)
-            {
-                case TileID.Fireplace:
-                case TileID.Campfire:
-                    HideBuffSystem.HasCampfire = true;
-                    break;
-                case TileID.HangingLanterns:
-                    if (item.placeStyle == 9)
-                        HideBuffSystem.HasHeartLantern = true;
-                    if (item.placeStyle == 7)
-                        HideBuffSystem.HasStarInBottle = true;
-                    break;
-                case TileID.Sunflower:
-                    HideBuffSystem.HasSunflower = true;
-                    break;
-                case TileID.GardenGnome:
-                    HideBuffSystem.HasGardenGnome = true;
-                    break;
-            }
-        }
+        #endregion
 
         private void BanBuffs(On.Terraria.Player.orig_AddBuff orig, Player player, int type, int timeToAdd, bool quiet, bool foodHack) {
             if (Main.myPlayer == player.whoAmI && DataPlayer.TryGet(player, out var dataPlayer)) {
@@ -173,7 +223,6 @@ namespace ImproveGame.Common.Players
             if (modBuff is null) { // 原版
                 if (!dataPlayer.InfBuffDisabledVanilla.Contains(buffType)) {
                     dataPlayer.InfBuffDisabledVanilla.Add(buffType);
-                    dataPlayer.InfBuffDisabledVanilla.Sort();
                 }
                 else {
                     dataPlayer.InfBuffDisabledVanilla.Remove(buffType);
@@ -183,7 +232,6 @@ namespace ImproveGame.Common.Players
                 string fullName = $"{modBuff.Mod.Name}/{modBuff.Name}";
                 if (!dataPlayer.InfBuffDisabledMod.Contains(fullName)) {
                     dataPlayer.InfBuffDisabledMod.Add(fullName);
-                    dataPlayer.InfBuffDisabledMod.Sort();
                 }
                 else {
                     dataPlayer.InfBuffDisabledMod.Remove(fullName);
