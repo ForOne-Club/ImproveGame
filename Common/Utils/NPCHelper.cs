369341e5982b774ae577da20217ecc24a9d06405
局长 <487666123@qq.com>
2023-10-04 13:11:16 +0800

    切换分支

---
+++ Common/Utils/NPCHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace ImproveGame.Common.Utils;
+
+public static class NPCHelper
+{
+    public static void SetBaseValues(this NPC npc, int width, int height, int lifeMax, bool friendly, int value, int damage = 0, int defense = 0, float knockBackResist = 0f)
+    {
+        npc.width = width;
+        npc.height = height;
+        npc.lifeMax = lifeMax;
+        npc.friendly = friendly;
+        npc.value = value;
+        npc.damage = damage;
+        npc.defense = defense;
+        npc.knockBackResist = knockBackResist;
+    }
+}
