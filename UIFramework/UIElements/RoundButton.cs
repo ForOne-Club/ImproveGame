29b5fa5eb4ce23c4fc2e478cc9a6fd946b1a688a
局长 <487666123@qq.com>
2024-02-09 01:43:02 +0800

    更换空间魔杖素材，改为默认 1f 图标大小

--- UIFramework/UIElements/RoundButton.cs
+++ UIFramework/UIElements/RoundButton.cs
@@ -1,85 +1,81 @@
-﻿namespace ImproveGame.UIFramework.UIElements
+﻿namespace ImproveGame.UIFramework.UIElements;
+
+public class RoundButton : UIElement
 {
-    public class RoundButton : UIElement
+    public static Texture2D BgTexture => ModAsset.Background.Value;
+    public static Texture2D HoverTexture => ModAsset.Hover.Value;
+
+    public Asset<Texture2D> MainTexture { get; set; }
+
+    public float Opacity;
+    public float IconScale = 1f;
+
+    public Func<bool> Selected;
+    public Func<string> text;
+
+    // 针不错, 全都加上渐变真不错!
+    public readonly AnimationTimer HoverTimer = new(3);
+    public readonly AnimationTimer SelectedTimer = new(3);
+
+    public string Text => text?.Invoke();
+
+    public RoundButton(Asset<Texture2D> mainImage)
     {
-        public Asset<Texture2D> background;
-        public Asset<Texture2D> hover;
-        public Asset<Texture2D> mainImage;
+        MainTexture = mainImage;
+        this.SetSize(BgTexture.Size());
+    }
 
-        public float Opacity;
-        public float IconScale = 0.8f;
+    public override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+        HoverTimer.Update();
+        SelectedTimer.Update();
+    }
 
-        public Func<bool> Selected;
-        public Func<string> text;
+    public override void MouseOver(UIMouseEvent evt)
+    {
+        base.MouseOver(evt);
+        HoverTimer.OpenAndResetTimer();
+    }
 
-        // 针不错, 全都加上渐变真不错!
-        public AnimationTimer HoverTimer;
-        public AnimationTimer SelectedTimer;
+    public override void MouseOut(UIMouseEvent evt)
+    {
+        base.MouseOut(evt);
+        HoverTimer.CloseAndResetTimer();
+    }
 
-        public string Text => text?.Invoke();
+    public Color GetColor()
+    {
+        if (Selected())
+            SelectedTimer.Open();
+        else
+            SelectedTimer.Close();
+        return Color.Lerp(Color.Gray, Color.White, SelectedTimer.Schedule);
+    }
 
-        public RoundButton(Asset<Texture2D> mainImage)
-        {
-            background = GetTexture("UI/Brust/Background");
-            hover = GetTexture("UI/Brust/Hover");
+    /*private readonly Color BorderColor1 = new(233, 176, 0, 200);
+    private readonly Color BorderColor2 = new(18, 18, 38, 200);
+    private readonly Color Background1 = new(83, 88, 151, 200);
+    private readonly Color Background2 = new(63, 65, 151, 200);*/
 
-            this.SetSize(background.Size());
-            this.mainImage = mainImage;
+    public override void DrawSelf(SpriteBatch sb)
+    {
+        CalculatedStyle dimensions = GetDimensions();
 
-            HoverTimer = new(3);
-            SelectedTimer = new(3);
-        }
+        Vector2 position = dimensions.Position() + this.GetSize() / 2f;
+        Color color = GetColor() * Opacity;
 
-        public override void Update(GameTime gameTime)
-        {
-            base.Update(gameTime);
-            HoverTimer.Update();
-            SelectedTimer.Update();
-        }
+        sb.Draw(BgTexture, position, null, color, 0, this.GetSize() / 2f, 1f, 0, 0f);
 
-        public override void MouseOver(UIMouseEvent evt)
-        {
-            base.MouseOver(evt);
-            HoverTimer.OpenAndResetTimer();
-        }
+        // color * 1.4f => 高光边框贴图应该亮一点
+        sb.Draw(HoverTexture, position, null, color * 1.4f * HoverTimer.Schedule, 0, this.GetSize() / 2f, 1f, 0, 0f);
 
-        public override void MouseOut(UIMouseEvent evt)
-        {
-            base.MouseOut(evt);
-            HoverTimer.CloseAndResetTimer();
-        }
+        /*Color borderColor = Color.Lerp(BorderColor2, BorderColor1, HoverTimer.Schedule);
+        Color background = Color.Lerp(Background2, Background1, SelectedTimer.Schedule);
 
-        public Color GetColor()
-        {
-            if (Selected())
-                SelectedTimer.Open();
-            else
-                SelectedTimer.Close();
-            return Color.Lerp(Color.Gray, Color.White, SelectedTimer.Schedule);
-        }
+        PixelShader.DrawBox(Main.UIScaleMatrix, GetDimensions().Position(),
+            this.GetSize(), Width.Pixels / 2, 3, borderColor * Opacity, background * Opacity);*/
 
-        /*private readonly Color BorderColor1 = new(233, 176, 0, 200);
-        private readonly Color BorderColor2 = new(18, 18, 38, 200);
-        private readonly Color Background1 = new(83, 88, 151, 200);
-        private readonly Color Background2 = new(63, 65, 151, 200);*/
-
-        public override void DrawSelf(SpriteBatch sb)
-        {
-            CalculatedStyle dimensions = GetDimensions();
-            Vector2 position = dimensions.Position() + this.GetSize() / 2f;
-            Color color = GetColor() * Opacity;
-
-            sb.Draw(background.Value, position, null, color, 0, this.GetSize() / 2f, 1f, 0, 0f);
-            // color * 1.4f => 高光边框贴图应该亮一点
-            sb.Draw(hover.Value, position, null, color * 1.4f * HoverTimer.Schedule, 0, this.GetSize() / 2f, 1f, 0, 0f);
-
-            /*Color borderColor = Color.Lerp(BorderColor2, BorderColor1, HoverTimer.Schedule);
-            Color background = Color.Lerp(Background2, Background1, SelectedTimer.Schedule);
-
-            PixelShader.DrawBox(Main.UIScaleMatrix, GetDimensions().Position(),
-                this.GetSize(), Width.Pixels / 2, 3, borderColor * Opacity, background * Opacity);*/
-
-            sb.Draw(mainImage.Value, position, null, color, 0, mainImage.Size() / 2f, IconScale, 0, 0f);
-        }
+        sb.Draw(MainTexture.Value, position, null, color, 0, MainTexture.Size() / 2f, IconScale, 0, 0f);
     }
 }
