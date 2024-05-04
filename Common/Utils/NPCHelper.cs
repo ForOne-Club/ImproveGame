c881c1145ce4d88271041b98ef505a2d2011b7ab
Cyrilly <59193481@qq.com>
2024-05-04 17:58:32 +0800

    自动重召唤修复 & 完善商店拓展支持

--- Common/Utils/NPCHelper.cs
+++ Common/Utils/NPCHelper.cs
@@ -2,7 +2,8 @@
 
 public static class NPCHelper
 {
-    public static void SetBaseValues(this NPC npc, int width, int height, int lifeMax, bool friendly, int value, int damage = 0, int defense = 0, float knockBackResist = 0f)
+    public static void SetBaseValues(this NPC npc, int width, int height, int lifeMax, bool friendly, int value,
+        int damage = 0, int defense = 0, float knockBackResist = 0f)
     {
         npc.width = width;
         npc.height = height;
@@ -13,4 +14,25 @@ public static class NPCHelper
         npc.defense = defense;
         npc.knockBackResist = knockBackResist;
     }
+
+    public static NPCShop Add<T>(this NPCShop shop, int customBuyPrice, params Condition[] condition)
+        where T : ModItem =>
+        shop.Add(new NPCShop.Entry(new Item(ModContent.ItemType<T>())
+        {
+            shopCustomPrice = customBuyPrice
+        }, condition));
+
+    public static NPCShop Add<T>(this NPCShop shop, Condition condition, int platinum = 0, int gold = 0, int silver = 0)
+        where T : ModItem =>
+        shop.Add(new NPCShop.Entry(new Item(ModContent.ItemType<T>())
+        {
+            shopCustomPrice = Item.buyPrice(platinum, gold, silver)
+        }, condition));
+
+    public static NPCShop Add<T>(this NPCShop shop, int platinum = 0, int gold = 0, int silver = 0)
+        where T : ModItem =>
+        shop.Add(new NPCShop.Entry(new Item(ModContent.ItemType<T>())
+        {
+            shopCustomPrice = Item.buyPrice(platinum, gold, silver)
+        }));
 }
