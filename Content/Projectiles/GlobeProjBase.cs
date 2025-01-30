9d3829329490c133beb3a1bc39d36dfdea96de8b
hezuo1hao <2124220174@qq.com>
2025-01-30 18:27:04 +0800

    优化顶点拖尾在死亡时的表现

--- Content/Projectiles/GlobeProjBase.cs
+++ Content/Projectiles/GlobeProjBase.cs
@@ -58,7 +58,8 @@ public abstract class GlobeProjBase : ModProjectile
             }
         }
 
-        if (Projectile.ai[2] < -180)
+        // ai进入二阶段
+        if (Projectile.ai[2] < -180 && Projectile.ai[1] >= 0)
         {
             Player player = Main.player[Projectile.owner];
 
@@ -73,9 +74,13 @@ public abstract class GlobeProjBase : ModProjectile
 
             Projectile.rotation = Projectile.velocity.ToRotation();
 
-            // 死亡判定与幽默特效
+            // 你给我死
             if (Projectile.owner == Main.myPlayer && Projectile.Center.Distance(player.Center) < 12)
-                Projectile.Kill();
+            {
+                Projectile.velocity *= 0.000000001f;
+                Projectile.ai[1] = -1;
+                NetMessage.SendData(MessageID.SyncProjectile, -1, -1, null, Projectile.whoAmI);
+            }
 
             // 幽默特效
             if (Projectile.ai[2] % 12 == 0)
@@ -84,6 +89,93 @@ public abstract class GlobeProjBase : ModProjectile
             if (Main.rand.NextBool(2))
                 Dust.NewDustPerfect(Projectile.Center + (Vector2.Normalize(Projectile.velocity) * -16), DustID.Smoke,
                     -Projectile.velocity.RotatedByRandom(0.3f) * 0.5f, 0, default, 2f);
+        }
+
+        // 死亡缓至——无法预料，无法逃避
+        if (Projectile.ai[1] == -1)
+        {
+            Projectile.ai[1]--;
+
+            // 物品只能在服务器/单人掉落
+            if (!RevealOperation(false) && Main.netMode is not NetmodeID.MultiplayerClient)
+                Main.player[Projectile.owner].QuickSpawnItem(Projectile.GetItemSource_DropAsItem(), GetModItemDummy().Type);
+
+            if (Projectile.ai[2] < 0)
+            {
+                SoundEngine.PlaySound(SoundID.Item14, Projectile.Center);
+
+                #region 别展开，里头有答辩
+
+                // 你 好
+                for (int i = 0; i < 10; i++)
+                {
+                    int dustIndex = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y),
+                        Projectile.width, Projectile.height, DustID.Smoke, 0f, 0f, 100, default, 2f);
+                    Main.dust[dustIndex].velocity *= 1.4f;
+                }
+
+                for (int i = 0; i < 20; i++)
+                {
+                    int dustIndex = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y),
+                        Projectile.width, Projectile.height, DustID.Torch, 0f, 0f, 100, default, 3f);
+                    Main.dust[dustIndex].noGravity = true;
+                    Main.dust[dustIndex].velocity *= 5f;
+                    dustIndex = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width,
+                        Projectile.height, DustID.Torch, 0f, 0f, 100, default, 2f);
+                    Main.dust[dustIndex].velocity *= 3f;
+                }
+
+                for (int g = 0; g < 2; g++)
+                {
+                    int goreIndex = Gore.NewGore(Projectile.GetSource_Death(),
+                        new Vector2(Projectile.position.X + Projectile.width / 2 - 24f,
+                            Projectile.position.Y + Projectile.height / 2 - 24f), default, Main.rand.Next(61, 64), 1f);
+                    Main.gore[goreIndex].velocity.X += 1.5f;
+                    Main.gore[goreIndex].velocity.Y += 1.5f;
+                    goreIndex = Gore.NewGore(Projectile.GetSource_Death(),
+                        new Vector2(Projectile.position.X + Projectile.width / 2 - 24f,
+                            Projectile.position.Y + Projectile.height / 2 - 24f), default, Main.rand.Next(61, 64), 1f);
+                    Main.gore[goreIndex].velocity.X -= 1.5f;
+                    Main.gore[goreIndex].velocity.Y += 1.5f;
+                    goreIndex = Gore.NewGore(Projectile.GetSource_Death(),
+                        new Vector2(Projectile.position.X + Projectile.width / 2 - 24f,
+                            Projectile.position.Y + Projectile.height / 2 - 24f), default, Main.rand.Next(61, 64), 1f);
+                    Main.gore[goreIndex].velocity.X += 1.5f;
+                    Main.gore[goreIndex].velocity.Y -= 1.5f;
+                    goreIndex = Gore.NewGore(Projectile.GetSource_Death(),
+                        new Vector2(Projectile.position.X + Projectile.width / 2 - 24f,
+                            Projectile.position.Y + Projectile.height / 2 - 24f), default, Main.rand.Next(61, 64), 1f);
+                    Main.gore[goreIndex].velocity.X -= 1.5f;
+                    Main.gore[goreIndex].velocity.Y -= 1.5f;
+                }
+
+                #endregion
+
+                return;
+            }
+
+            // 特效弹幕只能在服务器/单人生成
+            var effectColor = GetEffectColor();
+            if (Main.netMode is not NetmodeID.MultiplayerClient)
+                Projectile.NewProjectile(Projectile.GetSource_Death(), Projectile.Center, Vector2.Zero,
+                    ModContent.ProjectileType<GlobeEffect>(), 0, 0, ai0: effectColor.R, ai1: effectColor.G,
+                    ai2: effectColor.B);
+
+            SoundEngine.PlaySound(SoundID.Item107, Projectile.Center);
+            for (int i = 0; i < 15; i++)
+            {
+                Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Glass, 0f, -2f, 0, default,
+                    1.2f);
+            }
+        }
+
+        if (Projectile.ai[1] < 0)
+        {
+            Projectile.ai[1]--;
+
+            // 这才是真正的死亡
+            if (Projectile.ai[1] < -30)
+                Projectile.Kill();
         }
     }
 
@@ -107,82 +199,18 @@ public abstract class GlobeProjBase : ModProjectile
             return false;
         }
 
-        return base.OnTileCollide(oldVelocity);
-    }
-
-    public override void OnKill(int timeLeft)
-    {
-        // 物品只能在服务器/单人掉落
-        if (!RevealOperation(false) && Main.netMode is not NetmodeID.MultiplayerClient)
-            Main.player[Projectile.owner].QuickSpawnItem(Projectile.GetItemSource_DropAsItem(), GetModItemDummy().Type);
-
-        if (Projectile.ai[2] < 0)
+        // 进死门（
+        if (Projectile.ai[1] >= 0 && Projectile.owner == Main.myPlayer)
         {
-            SoundEngine.PlaySound(SoundID.Item14, Projectile.Center);
-
-            #region 别展开，里头有答辩
-
-            // 你 好
-            for (int i = 0; i < 10; i++)
-            {
-                int dustIndex = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y),
-                    Projectile.width, Projectile.height, DustID.Smoke, 0f, 0f, 100, default, 2f);
-                Main.dust[dustIndex].velocity *= 1.4f;
-            }
-
-            for (int i = 0; i < 20; i++)
-            {
-                int dustIndex = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y),
-                    Projectile.width, Projectile.height, DustID.Torch, 0f, 0f, 100, default, 3f);
-                Main.dust[dustIndex].noGravity = true;
-                Main.dust[dustIndex].velocity *= 5f;
-                dustIndex = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width,
-                    Projectile.height, DustID.Torch, 0f, 0f, 100, default, 2f);
-                Main.dust[dustIndex].velocity *= 3f;
-            }
-
-            for (int g = 0; g < 2; g++)
-            {
-                int goreIndex = Gore.NewGore(Projectile.GetSource_Death(),
-                    new Vector2(Projectile.position.X + Projectile.width / 2 - 24f,
-                        Projectile.position.Y + Projectile.height / 2 - 24f), default, Main.rand.Next(61, 64), 1f);
-                Main.gore[goreIndex].velocity.X += 1.5f;
-                Main.gore[goreIndex].velocity.Y += 1.5f;
-                goreIndex = Gore.NewGore(Projectile.GetSource_Death(),
-                    new Vector2(Projectile.position.X + Projectile.width / 2 - 24f,
-                        Projectile.position.Y + Projectile.height / 2 - 24f), default, Main.rand.Next(61, 64), 1f);
-                Main.gore[goreIndex].velocity.X -= 1.5f;
-                Main.gore[goreIndex].velocity.Y += 1.5f;
-                goreIndex = Gore.NewGore(Projectile.GetSource_Death(),
-                    new Vector2(Projectile.position.X + Projectile.width / 2 - 24f,
-                        Projectile.position.Y + Projectile.height / 2 - 24f), default, Main.rand.Next(61, 64), 1f);
-                Main.gore[goreIndex].velocity.X += 1.5f;
-                Main.gore[goreIndex].velocity.Y -= 1.5f;
-                goreIndex = Gore.NewGore(Projectile.GetSource_Death(),
-                    new Vector2(Projectile.position.X + Projectile.width / 2 - 24f,
-                        Projectile.position.Y + Projectile.height / 2 - 24f), default, Main.rand.Next(61, 64), 1f);
-                Main.gore[goreIndex].velocity.X -= 1.5f;
-                Main.gore[goreIndex].velocity.Y -= 1.5f;
-            }
-
-            #endregion
-
-            return;
+            Projectile.velocity *= 0.000000001f;
+            Projectile.tileCollide = false;
+            Projectile.ai[1] = -1;
+            Projectile.aiStyle = -1;
+            AIType = 0;
+            NetMessage.SendData(MessageID.SyncProjectile, -1, -1, null, Projectile.whoAmI);
         }
 
-        // 特效弹幕只能在服务器/单人生成
-        var effectColor = GetEffectColor();
-        if (Main.netMode is not NetmodeID.MultiplayerClient)
-            Projectile.NewProjectile(Projectile.GetSource_Death(), Projectile.Center, Vector2.Zero,
-                ModContent.ProjectileType<GlobeEffect>(), 0, 0, ai0: effectColor.R, ai1: effectColor.G,
-                ai2: effectColor.B);
-
-        SoundEngine.PlaySound(SoundID.Item107, Projectile.Center);
-        for (int i = 0; i < 15; i++)
-        {
-            Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Glass, 0f, -2f, 0, default,
-                1.2f);
-        }
+        return false;
     }
 
     public override bool PreDraw(ref Color lightColor)
@@ -199,19 +227,24 @@ public abstract class GlobeProjBase : ModProjectile
 
         // 顶点拖尾
         var triangleList = PrepareTriangleList();
-        DrawTrail(triangleList);
+        if (triangleList.Count >= 3)
+            DrawTrail(triangleList);
 
-        // 背后发光大圆球
-        Main.spriteBatch.Draw(roundGlow, center, null, glowCrossColor * 0.7f,
-            1.57f, roundGlow.Size() / 2f, 0.45f, SpriteEffects.None, 0);
+        // 进入死亡阶段后不要再绘制下面了
+        if (Projectile.ai[1] >= 0)
+        {
+            // 背后发光大圆球
+            Main.spriteBatch.Draw(roundGlow, center, null, glowCrossColor * 0.7f,
+                1.57f, roundGlow.Size() / 2f, 0.45f, SpriteEffects.None, 0);
 
-        // 本体绘制
-        Main.spriteBatch.Draw(projTex, center, null, Color.White,
-            Projectile.rotation + 1.57f, projTex.Size() / 2f, Projectile.scale, SpriteEffects.None, 0);
+            // 本体绘制
+            Main.spriteBatch.Draw(projTex, center, null, Color.White,
+                Projectile.rotation + 1.57f, projTex.Size() / 2f, Projectile.scale, SpriteEffects.None, 0);
 
-        // 十字高光绘制
-        Main.spriteBatch.Draw(cross, center, null, glowCrossColor,
-            0f, cross.Size() / 2f, 1f, SpriteEffects.None, 0);
+            // 十字高光绘制
+            Main.spriteBatch.Draw(cross, center, null, glowCrossColor,
+                0f, cross.Size() / 2f, 1f, SpriteEffects.None, 0);
+        }
 
         Main.spriteBatch.ReBegin(SpriteSortMode.Deferred, BlendState.AlphaBlend);
 
