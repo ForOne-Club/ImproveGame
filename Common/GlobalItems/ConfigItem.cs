95a546a223a9809e0012fa74ba15f140a40786df
太阳照常升起 <487666123@qq.com>
2025-04-26 22:40:04 +0800

    魔杖禁用的 X 图标换成原版药水病的， 1817 更新日志

--- Common/GlobalItems/ConfigItem.cs
+++ Common/GlobalItems/ConfigItem.cs
@@ -20,8 +20,8 @@
                 // 如果有更好的图片替换这里就行了
                 // 原版 UI 绘制的地方尽量不使用 SDF，除非这个地方没有使用合批（基本没这个情况）
                 // SDF 通常用于一个单独的空间或者 UI 的背景绘制
-                var texture2d = ModAsset.Images_Close.Value;
-                spriteBatch.Draw(texture2d, position, null, Color.White * 0.75f, 0f, new Vector2(texture2d.Width, texture2d.Height) / 2f, 1f, 0, 0f);
+                var texture2d = ModAsset.CoolDown.Value;
+                spriteBatch.Draw(texture2d, position, null, Color.White * 0.75f, 0f, new Vector2(texture2d.Width, texture2d.Height) / 2f, scale, 0, 0f);
             }
         }
     }
