e94f34d153b06a083e7c9b8093d53627c1a9c54d
太阳照常升起 <487666123@qq.com>
2025-09-08 04:00:56 +0800

    进度推进

---
+++ UserInterfaces/Global/PromptUI.cs
@@ -0,0 +1,30 @@
+﻿#if false
+
+using SilkyUIFramework;
+using SilkyUIFramework.Attributes;
+using SilkyUIFramework.BasicElements;
+
+namespace ImproveGame.UserInterfaces.Global;
+
+[RegisterGlobalUI("PromptUI", 0)]
+public partial class PromptUI : BasicBody
+{
+    protected override void OnInitialize()
+    {
+        InitializeComponent();
+
+        Title.UseDeathText();
+
+        BorderColor = SUIColor.Border;
+        BackgroundColor = SUIColor.Background * 0.75f;
+
+        BorderRadius = new Vector4(4);
+    }
+
+    protected override void UpdateStatus(GameTime gameTime)
+    {
+        base.UpdateStatus(gameTime);
+    }
+}
+
+#endif
---
+++ UserInterfaces/InfiniteBUFFController/InfiniteBUFFController.cs
@@ -0,0 +1,47 @@
+﻿using SilkyUIFramework;
+using SilkyUIFramework.Attributes;
+using SilkyUIFramework.BasicElements;
+
+namespace ImproveGame.UserInterfaces.InfiniteBUFFController;
+
+/// <summary>
+/// 设计中不要动
+/// </summary>
+[RegisterUI("Vanilla: Radial Hotbars", "InfiniteBUFFController")]
+public partial class InfiniteBUFFController : BasicBody
+{
+    public UIElementGroup ScrollContainer { get; private set; }
+
+    protected override void OnInitialize()
+    {
+        Enabled = false;
+
+        BorderColor = SUIColor.Border * 0.75f;
+        BackgroundColor = SUIColor.Background * 0.75f;
+
+        InitializeComponent();
+        ScrollContainer = ScrollView.Container;
+
+        Header.ControlTarget = this;
+        Title.UseDeathText();
+
+        Close.Texture2D = Main.Assets.Request<Texture2D>("Images/UI/SearchCancel");
+        Close.OnUpdateStatus += delegate
+        {
+            Close.ImageColor = Color.White * Close.HoverTimer.Lerp(0.5f, 1f);
+        };
+
+        ClearEditText.Texture2D = Main.Assets.Request<Texture2D>("Images/UI/SearchCancel");
+        ClearEditText.OnUpdateStatus += delegate
+        {
+            ClearEditText.ImageColor = Color.White * ClearEditText.HoverTimer.Lerp(0.5f, 1f);
+        };
+
+        ClearEditText.LeftMouseDown += delegate { FilterBox.Text = ""; };
+    }
+
+    protected override void UpdateStatus(GameTime gameTime)
+    {
+        base.UpdateStatus(gameTime);
+    }
+}
