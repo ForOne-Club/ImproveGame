a4c46e9ce3de8ca3518d9938397f2ab6c56257ac
太阳照常升起 <487666123@qq.com>
2026-04-14 03:38:29 +0800

    DefaultStyle 继承 ObservableObject

--- Styles/DefaultStyle.cs
+++ Styles/DefaultStyle.cs
@@ -1,12 +1,16 @@
-﻿using SilkyUIFramework;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using SilkyUIFramework;
 
 namespace ImproveGame.Styles;
 
 /// <summary>
 /// Qot 基础样式（扩展中），基于 SilkyUI 开发的所有 UI 样式以此为基类
 /// </summary>
-public class DefaultStyle
+public partial class DefaultStyle : ObservableObject
 {
-    public Color BorderColor { get; set; } = SUIColor.Border;
-    public Color BackgroundColor { get; set; } = SUIColor.Background * 0.75f;
+    [ObservableProperty]
+    public partial Color BorderColor { get; set; } = SUIColor.Border;
+
+    [ObservableProperty]
+    public partial Color BackgroundColor { get; set; } = SUIColor.Background * 0.75f;
 }
